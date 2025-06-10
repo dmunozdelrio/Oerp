29a2cb85133d5b9aabc319b7f4eee9711e664a05
Diego Munoz del rio <85699761+dmunozdelrio@users.noreply.github.com>
2025-06-10 17:40:03 -0600

    Use gl change log sequence in header trigger

--- triggers/tr_gl_header_log.sql
+++ triggers/tr_gl_header_log.sql
@@ -9,13 +9,13 @@ FOR EACH ROW
 BEGIN
   IF INSERTING THEN
     INSERT INTO gl_change_log(log_id, table_name, operation, row_id_val, changed_by, changed_date)
-    VALUES(sq_gl_header.NEXTVAL, 'GL_HEADER', 'INSERT', :NEW.glh_id, USER, SYSDATE);
+    VALUES(sq_gl_change_log.NEXTVAL, 'GL_HEADER', 'INSERT', :NEW.glh_id, USER, SYSDATE);
   ELSIF UPDATING THEN
     INSERT INTO gl_change_log(log_id, table_name, operation, row_id_val, changed_by, changed_date)
-    VALUES(sq_gl_header.NEXTVAL, 'GL_HEADER', 'UPDATE', :OLD.glh_id, USER, SYSDATE);
+    VALUES(sq_gl_change_log.NEXTVAL, 'GL_HEADER', 'UPDATE', :OLD.glh_id, USER, SYSDATE);
   ELSIF DELETING THEN
     INSERT INTO gl_change_log(log_id, table_name, operation, row_id_val, changed_by, changed_date)
-    VALUES(sq_gl_header.NEXTVAL, 'GL_HEADER', 'DELETE', :OLD.glh_id, USER, SYSDATE);
+    VALUES(sq_gl_change_log.NEXTVAL, 'GL_HEADER', 'DELETE', :OLD.glh_id, USER, SYSDATE);
   END IF;
 END;
 /
